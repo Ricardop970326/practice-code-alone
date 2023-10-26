82b8a46648ad75b1ee98d4f1bdc376089467c9c5
Ricardop970326 <ricardop.alejandrog@gmail.com>
2023-10-26 14:55:47 -0400

    update

--- speech-reconnition/script.js
+++ speech-reconnition/script.js
@@ -2,5 +2,26 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recognition = new SpeechRecognition();
 
-recognition.intermResult = true;
+recognition.intermResults = true;
 recognition.lang = 'en-US';
+
+const words = document.querySelector('.words');
+let p = document.createElement('p');
+words.append(p);
+
+recognition.addEventListener('result', e => {
+    // console.log(e).results;
+    const transcript = Array.from (e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+    p.textContent = transcript;
+
+    if (e.results[0].isFinal){
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+})
+
+recognition.addEventListener('end', recognition.start);
+recognition.start();
